4c405b20a9e7aa6de8245ad1dfb226c32e67acea
hytor.yang <hytor.yang@gmail.com>
2014-11-12 20:56:55 +0800

    process post text

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ function start(route, handle) {
 
 	function onRequest(request, response) {
 
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 
-	  	route(handle, pathname, response);
+		request.setEncoding("utf8");
+
+		request.addListener('data', function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received post data chunck '" + postDataChunk + "'.");
+		});
+
+		request.addListener('end', function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(8888);
